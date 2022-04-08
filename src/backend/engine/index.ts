6f8254acc642e4a319b5bce3f53e998c89fe0743
James Hoff <jameshoff@microsoft.com>
2022-04-08 16:03:46 -0400

    update

--- src/backend/engine/index.ts
+++ src/backend/engine/index.ts
@@ -7,39 +7,36 @@ export class BpaEngine {
     }
 
     public processFile = async (fileBuffer: Buffer, fileName: string, config: BpaConfiguration) => {
-        try {
-            let currentInput: BpaServiceObject = {
-                label: "first",
-                type: this._getFileType(fileName),
-                projectName: "project",
-                data: fileBuffer,
-                bpaId: "1"
-            }
 
-            console.log(this._getFileType(fileName))
+        let currentInput: BpaServiceObject = {
+            label: "first",
+            type: this._getFileType(fileName),
+            projectName: "project",
+            data: fileBuffer,
+            bpaId: "1"
+        }
 
-            for (const stage of config.stages) {
-                console.log(`stage : ${stage.service.name}`)
-                console.log('validating...')
-                if (this._validateInput(currentInput.type, stage)) {
-                    console.log('validation passed!!')
-                    console.log('processing....')
-                    currentInput.serviceSpecificConfig = stage.service.serviceSpecificConfig
-                    const currentOutput: BpaServiceObject = await stage.service.process(currentInput)
-                    console.log('exiting stage')
-                    currentInput = currentOutput
-                }
-                else {
-                    throw new Error(`invalid input type ${currentInput} for stage ${stage.service.name}`)
-                }
+        console.log(this._getFileType(fileName))
+
+        for (const stage of config.stages) {
+            console.log(`stage : ${stage.service.name}`)
+            console.log('validating...')
+            if (this._validateInput(currentInput.type, stage)) {
+                console.log('validation passed!!')
+                console.log('processing....')
+                currentInput.serviceSpecificConfig = stage.service.serviceSpecificConfig
+                const currentOutput: BpaServiceObject = await stage.service.process(currentInput)
+                console.log('exiting stage')
+                currentInput = currentOutput
             }
-        } catch (err) {
-            console.log(err)
+            else {
+                throw new Error(`invalid input type ${currentInput} for stage ${stage.service.name}`)
+            }
         }
     }
 
     private _validateInput = (input: string, stage: BpaStage): boolean => {
-        if(stage.service.name == 'view'){
+        if (stage.service.name == 'view') {
             return true
         }
         if (stage.service.inputTypes.includes(input)) {
